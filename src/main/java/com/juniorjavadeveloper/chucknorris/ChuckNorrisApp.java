3d62536bfa0863dad15b7fcb8f9ae29a10ea6afc
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2023-03-08 11:21:35 +0700

    test deploy

---
+++ src/main/java/com/juniorjavadeveloper/chucknorris/ChuckNorrisApp.java
@@ -0,0 +1,13 @@
+package com.juniorjavadeveloper.chucknorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/juniorjavadeveloper/chucknorris/ChuckNorrisAppTests.java
@@ -0,0 +1,13 @@
+package com.juniorjavadeveloper.chucknorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
